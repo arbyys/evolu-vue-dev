0f541cf1e9cd006db1d9ee100a6a92a8254a97e0
arbyys <adam.petricek@centrum.cz>
2024-11-18 18:41:02 +0100

    evolu-provider

--- src/evolu-provider.ts
+++ src/evolu-provider.ts
@@ -1,5 +1,5 @@
 import { provide, inject } from 'vue';
-import type { Evolu } from '@evolu/common';
+import type { Evolu, EvoluSchema } from '@evolu/common';
 
 const EvoluSymbol = Symbol('Evolu');
 
@@ -7,10 +7,10 @@ export const provideEvolu = (evolu: Evolu<any>): void => {
   provide(EvoluSymbol, evolu);
 };
 
-export const useEvoluInstance = <T>(): Evolu<T> => {
-  const evolu = inject(EvoluSymbol) as Evolu<T>;
+export const useEvoluInstance = <S extends EvoluSchema>(): Evolu<S> => {
+  const evolu = inject(EvoluSymbol);
   if (!evolu) {
-    throw new Error('Evolu instance not found. Make sure to call `provideEvolu` at the app level.');
+    throw new Error('Could not inject Evolu instance. Make sure to call `provideEvolu` at the app level.');
   }
-  return evolu;
+  return evolu as Evolu<S>;
 };
