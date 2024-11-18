c65ce353fc8e553f98c436e9e22919d36dff0a44
arbyys <adam.petricek@centrum.cz>
2024-11-18 18:37:53 +0100

    useEvoluError

--- src/composables/useEvoluError.ts
+++ src/composables/useEvoluError.ts
@@ -4,7 +4,7 @@ import type { EvoluError } from '@evolu/common';
 
 export const useEvoluError = (): Ref<EvoluError | null> => {
   const evolu = useEvoluInstance();
-  const error = ref<EvoluError | null>(null);
+  const error = ref<EvoluError | null>(evolu.getError());
 
   const unsubscribe = evolu.subscribeError(() => {
     error.value = evolu.getError();
