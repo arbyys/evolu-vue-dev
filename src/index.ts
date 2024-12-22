543811fe3fd2c08d53ccb86e5e671faecac73818
arbyys <adam.petricek@centrum.cz>
2024-12-22 20:23:39 +0100

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 export { provideEvolu, injectEvolu } from './evolu-provider';
-export { useQuery } from './composables/useQuery';
-//export { useQueries } from './composables/useQueries';
 export { useEvoluError } from './composables/useEvoluError';
-export { useSyncState } from './composables/useSyncState';
 export { useOwner } from './composables/useOwner';
+export { useQueries } from './composables/useQueries';
+export { useQuery } from './composables/useQuery';
+export { useQuerySubscription } from './composables/useQuerySubscription';
+export { useSyncState } from './composables/useSyncState';
 
-import { Evolu, EvoluFactory, FlushSync } from "@evolu/common";
-export { createEvolu } from "@evolu/common-web";
 export * from "@evolu/common/public";
-
-//export { useQuerySubscription } from './composables/useQuerySubscription';
+export { createEvolu } from "@evolu/common-web";
+export { Evolu } from "@evolu/common";
 
 // add export of ./evolu-provider/useEvoluInstance renamed to useEvolu
 
